85f0404c179ab2b7d4b352587a417797cb704c7d
Milanvdm <milanvdm@hotmail.com>
2016-01-02 16:34:12 +0100

    changed folders

---
+++ medical-lstm/src/main/java/com/java/medical_lstm/medical_lstm/App.java
@@ -0,0 +1,13 @@
+package com.java.medical_lstm.medical_lstm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
